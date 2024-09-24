82d329744f5f1550c0a2a6f59023c1eee5e4031e
MaxNguyen4 <s4018839@student.rmit.edu.au>
2024-09-24 16:25:58 +1000

    added tailwind lib

---
+++ src/main/frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/main/java/com/example/spring_boot/resources/templates/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
