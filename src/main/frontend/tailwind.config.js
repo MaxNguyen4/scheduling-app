af4e6b82b52592cb4312bda09893eb057a8de005
MaxNguyen4 <s4018839@student.rmit.edu.au>
2024-09-24 17:25:46 +1000

    fixed tailwind implementation

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/java/com/example/spring_boot/resources/templates/**/*.{html,js}"],
+  content: ["../resources/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
--- src/main/java/com/example/spring_boot/controllers/HomeController.java
+++ src/main/java/com/example/spring_boot/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.spring_boot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
-@RestController
+
+@Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public String index(Model model) {
+		return "home";
 	}
 
 }
